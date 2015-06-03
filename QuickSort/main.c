1b86a63d9a23f467886c44142ba0464493d11abd
HoorayLee <leehorray@gmail.com>
2015-06-03 09:33:59 +0800

    Initial Commit

---
+++ QuickSort/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  QuickSort
+//
+//  Created by 洪锐 李 on 15/6/3.
+//  Copyright (c) 2015年 洪锐 李. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
